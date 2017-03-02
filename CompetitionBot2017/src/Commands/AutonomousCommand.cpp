271c64b8ce2bba93f7a57e7ccfdb2f0d37b363d2
comnlink <aidanroysadouskijames@gmail.com>
2017-03-01 16:49:33 -0800

    Update AutonomousCommand.cpp
    comments

--- CompetitionBot2017/src/Commands/AutonomousCommand.cpp
+++ CompetitionBot2017/src/Commands/AutonomousCommand.cpp
@@ -1,6 +1,6 @@
-#include "AutonomousCommand.h"
+#include "AutonomousCommand.h"// I really shouldn't have to comment include statements
 
-#include "DriveByDistance.h"
+#include "DriveByDistance.h"//but these are important
 #include "TurnByAngle.h"
 #include "FullAutoPlaceGear.h"
 
@@ -24,8 +24,8 @@ AutonomousCommand::AutonomousCommand(int id) {
 
 	AddSequential(new DriveByDistance(70.0));
 
-	if (id == REDL||id == BLUEL) {
-		AddSequential(new TurnByAngle(30.0));
+	if (id == REDL||id == BLUEL) {//fun fact these are defined in the include statements 
+		AddSequential(new TurnByAngle(30.0));//all of this is just taking the stuff we've defined the .h things and using it
 		AddSequential(new DriveByDistance(10.0));
 	} else if (id == REDR||id == REDL) {
 		AddSequential(new TurnByAngle(-30.0));
