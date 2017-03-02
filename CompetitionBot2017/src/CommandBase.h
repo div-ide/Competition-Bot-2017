934c66bafd816f4f7f9ed58c773aa618118100cd
Diego Soto <dashito.soto@gmail.com>
2017-03-01 17:22:39 -0800

    Update CommandBase.h

--- CompetitionBot2017/src/CommandBase.h
+++ CompetitionBot2017/src/CommandBase.h
@@ -2,10 +2,10 @@
 #define COMMAND_BASE_H //we define the title of the code to be used in other codes outside this sheet (in this case we define the name as
 			//COMMAND_BASE_H)
 
-#include <memory> //we tell the programn to remember this programming even if we are in a different code
+#include <memory> //we tell the program to remember this programming even if we are in a different code
 #include <string>
 
-#include <Commands/Command.h> //We include a programme that within it's code it's gonna have something tha affects this code as well
+#include <Commands/Command.h> //We include a program that within it's code it's gonna have something tha affects this code as well
 
 #include "OI.h"//same as above for these commands right here
 #include "Subsystems/Winch.h"
