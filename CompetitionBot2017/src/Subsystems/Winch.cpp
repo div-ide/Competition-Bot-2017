e057a02b4d1b81841c808ae910bfd599f6e4ab99
div-ide <cyrus.miller@gulfislandssecondary.ca>
2017-03-08 18:17:18 -0800

    Update Winch.cpp

--- CompetitionBot2017/src/Subsystems/Winch.cpp
+++ CompetitionBot2017/src/Subsystems/Winch.cpp
@@ -9,15 +9,19 @@ void Winch::InitDefaultCommand() {
 	//SetDefaultCommand(new ClimbRope());
 }
 
+
+// power is motor speed
 void Winch::SetMotorPower(double power) {
+	// if not defined, define variables
 	if (!definedYet) {
 		winchMotor = new frc::VictorSP(WINCH_MOTOR);
 		definedYet = true;
 	}
+	// update winch motor & dashboard
 	winchMotor->Set(-power);
 	frc::SmartDashboard::PutNumber("Winch Power", power);
 }
-
+// reset motor
 void Winch::Reset() {
 	winchMotor->Set(0.0);
 }
